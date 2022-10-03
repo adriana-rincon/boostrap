a65160f60e3fac35c4a1c6d3caf5f2ffdd43190d
adriana-rincon <110684189+adriana-rincon@users.noreply.github.com>
2022-10-03 11:01:13 -0500

    Add files via upload
    se presenta carpeta Bootstrap con variedad de contenidos y paginas con las que se visualizan maquetas con deferentes formas colores y tamaños, las paginas son de ventas y cards

---
+++ boostrap/js/ddds.js
@@ -0,0 +1,13 @@
+let product=[];
+let total=0
+
+    function add (product, prince){
+        console.log (product, price);
+        products. push(product);
+        total=total + price;
+        document.getElementById("checkout").innerHTML= {total}
+    }
+    function pay(){
+        console.log(product, price);
+        window.alert(products.join(", /n"));
+    }
